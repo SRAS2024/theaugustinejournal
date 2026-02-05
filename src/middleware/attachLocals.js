f40d7f599bd86c585f41b901af881b04564d8128
RSimonds <samuelrasimonds@gmail.com>
2026-02-04 17:46:57 -0700

    Add attachLocals middleware to set response locals

---
+++ src/middleware/attachLocals.js
@@ -0,0 +1,7 @@
+export function attachLocals(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  res.locals.isAdmin = Boolean(req.session?.admin?.isLoggedIn);
+  res.locals.adminName = req.session?.admin?.name || "";
+  res.locals.siteTitle = "The Augustine Journal";
+  next();
+}
