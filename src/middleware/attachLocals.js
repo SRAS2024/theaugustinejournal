42683a286ae60deff2a1f0d0e4cf2b3dd45ac80a
RSimonds <samuelrasimonds@gmail.com>
2026-02-06 17:55:17 -0700

    Safeguard csrfToken assignment in attachLocals
    Add check for csrfToken existence before assignment

--- src/middleware/attachLocals.js
+++ src/middleware/attachLocals.js
@@ -1,5 +1,7 @@
 export function attachLocals(req, res, next) {
-  res.locals.csrfToken = req.csrfToken();
+  // CSRF middleware is only enabled when DATABASE_URL is configured.
+  // If it is not enabled, req.csrfToken will not exist.
+  res.locals.csrfToken = typeof req.csrfToken === "function" ? req.csrfToken() : "";
   res.locals.isAdmin = Boolean(req.session?.admin?.isLoggedIn);
   res.locals.adminName = req.session?.admin?.name || "";
   res.locals.siteTitle = "The Augustine Journal";
